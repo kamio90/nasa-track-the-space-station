c7d0cab0c2e493f4a7c041f41ce0959d45825631
Kamil Musiał <kmusial40@gmail.com>
2022-10-01 16:27:13 +0200

    working fbx

--- src/index.js
+++ src/index.js
@@ -61,7 +61,9 @@ const mars = new Planet(
   "https://solartextures.b-cdn.net/2k_jupiter.jpg"
 );
 
-earth._LoadFBXModel("../static/models/sphere.fbx");
+earth._LoadFBXModel(
+  "https://threejs.org/examples/models/fbx/Samba%20Dancing.fbx"
+);
 // scene.addObjToScene(earthMesh, earth.getID, earth.getName());
 // scene.addObjToScene(marsMesh, mars.getID, mars.getName());
 
