33d7e0c152c968d7988f7421f3fce18cad074a6e
Monoloki <Rugall1998@gmail.com>
2022-10-01 20:04:06 +0200

    update

--- src/index.js
+++ src/index.js
@@ -30,7 +30,8 @@ const gridHelper = new THREE.GridHelper( size, divisions );
 //scene.addObjToScene( gridHelper );
 
 sun.setupScene(scene);
-sun._LoadFBXModel("http://localhost:8000/sun.fbx", 0.00001);
+//sun._LoadFBXModel("http://localhost:8000/sun.fbx", 0.00001);
+sun._LoadFBXModel("http://localhost:8000/stacja_test.fbx", 0.0001);
 mercury.setupScene(scene);
 mercury._LoadFBXModel("http://localhost:8000/mercury.fbx", 0.1);
 venus.setupScene(scene);
