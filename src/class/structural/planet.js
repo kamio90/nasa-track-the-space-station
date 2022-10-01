f3da00ef8e654e22a00c8eedf88a67629cbb27cc
Kamil Musiał <kmusial40@gmail.com>
2022-10-01 18:06:02 +0200

    move fbx fix

--- src/class/structural/planet.js
+++ src/class/structural/planet.js
@@ -1,9 +1,19 @@
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { ThirdDimensionVector } from "../types/third-dimension-vector";
 import { SpaceObject } from "./space-object";
 
 export class Planet extends SpaceObject {
-  constructor(id, name, wordPosition, wordRotation, wordPath, webLinks) {
+  constructor(
+    id,
+    name,
+    wordPosition,
+    wordRotation,
+    wordPath,
+    webLinks,
+    vector = new ThirdDimensionVector(0, 0, 0)
+  ) {
     super(id, name, wordPosition, wordRotation, wordPath, webLinks);
+    this.position = vector;
   }
 
   //Initializations
@@ -22,7 +32,20 @@ export class Planet extends SpaceObject {
     const fbxLoader = new FBXLoader();
     fbxLoader.load(url, (obj) => {
       obj.scale.multiplyScalar(scale);
-      this.scene.addObjToScene(obj);
+      obj.position.x = this.position.getX();
+      obj.position.y = this.position.getY();
+      obj.position.z = this.position.getZ();
+      obj.updateMatrix();
+      this.mesh = obj;
+      this.scene.addObjToScene(this.mesh);
     });
+  }
+
+  movePlanet(vector) {
+    this.position = this.position.addThirdDimensionVector(vector);
+    this.mesh.position.x = this.position.getX();
+    this.mesh.position.y = this.position.getY();
+    this.mesh.position.z = this.position.getZ();
+    this.mesh.updateMatrix();
   }
 }
