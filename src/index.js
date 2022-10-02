24c762ef5b926e389720832fee836362b3fa3bef
Kamil Musiał <kmusial40@gmail.com>
2022-10-02 03:44:41 +0200

    ui

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import "./style/main.css";
  * GUI Controls
  */
 import * as dat from "dat.gui";
-import { FetchData } from "./api/fetch-data";
+import { ISS } from "./class/structural/iss";
 import { CustomCamera } from "./class/threejs/custom-camera";
 import { CustomControls } from "./class/threejs/custom-controlls";
 import { CustomRenderer } from "./class/threejs/custom-renderer";
 import { CustomScene } from "./class/threejs/custom-scene";
+import { ThirdDimensionVector } from "./class/types/third-dimension-vector";
 import { earth } from "./data/planets/earth";
 import { jupiter } from "./data/planets/jupiter";
 import { mars } from "./data/planets/mars";
@@ -19,14 +20,20 @@ import { saturn } from "./data/planets/saturn";
 import { sun } from "./data/planets/sun";
 import { uranus } from "./data/planets/uranus";
 import { venus } from "./data/planets/venus";
-import { ISS } from "./class/structural/iss";
-import { ThirdDimensionVector } from "./class/types/third-dimension-vector";
 
 const gui = new dat.GUI();
 
+//FRONTEND - UI
+const menuBtn = document.querySelector(".left-nav-bar-btn-menu");
+menuBtn.addEventListener("click", () => {
+  const navBar = document.querySelector(".left-nav-bar-container");
+  menuBtn.classList.toggle("show");
+  navBar.classList.toggle("show");
+});
+
 //api call
-FetchData();
-setInterval(FetchData, 30000);
+// FetchData();
+// setInterval(FetchData, 30000);
 
 const canvas = document.querySelector("canvas.webgl");
 const scene = new CustomScene();
@@ -68,49 +75,192 @@ scene.addObjToScene(lights[1]);
 
 var iss = new ISS();
 
-iss.loadPartOfModel(scene,'http://localhost:8000/station/zvezda.fbx',0.1,new ThirdDimensionVector(0,100,0),'zvezda');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/zarya_chyba.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/arrayport1.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/arrayport2.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/arraystar1.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/arraystar2.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/columbus.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/destiny_USA.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/ELC.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/ELC2.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/frame.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/harmony_mod.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/IDA-2.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/idkman.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/isscombine_idk.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/jem_elm_ps.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/JEM_RMS&E.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/Kibo_JEM.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/Nauka_Lab.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/P1truss_segment.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/PMA-2.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/S1truss_segment.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/unity_node.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-iss.loadPartOfModel(scene,'http://localhost:8000/station/unity_node1.fbx',0.1,new ThirdDimensionVector(0,100,0),'zarya');
-
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/zvezda.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zvezda"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/zarya_chyba.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/arrayport1.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/arrayport2.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/arraystar1.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/arraystar2.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/columbus.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/destiny_USA.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/ELC.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/ELC2.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/frame.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/harmony_mod.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/IDA-2.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/idkman.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/isscombine_idk.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/jem_elm_ps.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/JEM_RMS&E.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/Kibo_JEM.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/Nauka_Lab.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/P1truss_segment.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/PMA-2.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/S1truss_segment.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/unity_node.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
+iss.loadPartOfModel(
+  scene,
+  "http://localhost:8000/station/unity_node1.fbx",
+  0.1,
+  new ThirdDimensionVector(0, 100, 0),
+  "zarya"
+);
 
 /**
  * Sizes
  */
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+  width: window.innerWidth - window.innerWidth * 0.7,
+  height: window.innerHeight - window.innerHeight * 0.7,
 };
 
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
+// window.addEventListener("resize", () => {
+//   // Update sizes
+//   sizes.width = window.innerWidth;
+//   sizes.height = window.innerHeight;
 
-  // Update camera
-  camera.getCamera().aspect = sizes.width / sizes.height;
-  camera.getCamera().updateProjectionMatrix();
-});
+//   // Update camera
+//   camera.getCamera().aspect = sizes.width / sizes.height;
+//   camera.getCamera().updateProjectionMatrix();
+// });
 
 /**
  * Camera
