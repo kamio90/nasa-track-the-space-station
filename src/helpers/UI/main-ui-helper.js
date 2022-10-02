9ca95537f098ff722d26ca4fb3b9260dbd0045de
Kamil Musiał <kmusial40@gmail.com>
2022-10-02 08:41:13 +0200

    export ui methods

---
+++ src/helpers/UI/main-ui-helper.js
@@ -0,0 +1,26 @@
+import { MonthsEnum } from "../../data/time/months-enum";
+import { YearsEnum } from "../../data/time/years-enum";
+import { CreateDivHelper } from "./create-div-helper";
+export const MainUIHelper = () => {
+  const btn = document.querySelector(".left-nav-bar-btn-menu");
+  btn.addEventListener("click", () => {
+    const nav = document.querySelector(".left-nav-bar-container");
+    btn.classList.toggle("show");
+    nav.classList.toggle("show");
+  });
+  const arrayOfPartsOfYear = [];
+  for (const property in YearsEnum) {
+    arrayOfPartsOfYear.push(CreateDivHelper(YearsEnum[property]));
+    document.querySelector(".year-holder").innerHTML = CreateDivHelper(
+      YearsEnum[property]
+    );
+  }
+  document.querySelector(".year-holder").innerHTML =
+    arrayOfPartsOfYear.join("");
+  const arrayOfPartsOfMonths = [];
+  for (const property in MonthsEnum) {
+    arrayOfPartsOfMonths.push(CreateDivHelper(property));
+  }
+  document.querySelector(".month-holder").innerHTML =
+    arrayOfPartsOfMonths.join("");
+};
--- src/index.js
+++ src/index.js
@@ -20,33 +20,12 @@ import { saturn } from "./data/planets/saturn";
 import { sun } from "./data/planets/sun";
 import { uranus } from "./data/planets/uranus";
 import { venus } from "./data/planets/venus";
-import { MonthsEnum } from "./data/time/months-enum";
-import { YearsEnum } from "./data/time/years-enum";
-import { CreateDivHelper } from "./helpers/UI/create-div-helper";
+import { MainUIHelper } from "./helpers/UI/main-ui-helper";
 
 const gui = new dat.GUI();
 
 //FRONT - UI
-const btn = document.querySelector(".left-nav-bar-btn-menu");
-btn.addEventListener("click", () => {
-  const nav = document.querySelector(".left-nav-bar-container");
-  btn.classList.toggle("show");
-  nav.classList.toggle("show");
-});
-const arrayOfPartsOfYear = [];
-for (const property in YearsEnum) {
-  arrayOfPartsOfYear.push(CreateDivHelper(YearsEnum[property]));
-  document.querySelector(".year-holder").innerHTML = CreateDivHelper(
-    YearsEnum[property]
-  );
-}
-document.querySelector(".year-holder").innerHTML = arrayOfPartsOfYear.join("");
-const arrayOfPartsOfMonths = [];
-for (const property in MonthsEnum) {
-  arrayOfPartsOfMonths.push(CreateDivHelper(property));
-}
-document.querySelector(".month-holder").innerHTML =
-  arrayOfPartsOfMonths.join("");
+MainUIHelper();
 
 //api call
 // FetchData();
